cce061fbf0b2829c8374f7b6757d3f0b7cbfffe9
shinseongsu <tlstjdtn321@naver.com>
2026-06-18 01:22:19 +0900

    fix: remove double CSS reset, add font preconnect hints

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ko">
       <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" />
         <link
           href="https://fonts.googleapis.com/css2?family=Nanum+Myeongjo&display=swap"
           rel="stylesheet"
